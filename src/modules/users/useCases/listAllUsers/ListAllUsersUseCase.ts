dbb28a8c3c77e12850501abb19603dc37cffc4d7
alisonrocha <alisonrochasilva@hotmail.com>
2022-01-05 11:47:47 -0300

    feito o desafio com sucesso

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,16 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute({ user_id }): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User not exists");
+
+    if (!user.admin) throw new Error("User is not an administrator");
+
+    return this.usersRepository.list();
   }
 }
 
